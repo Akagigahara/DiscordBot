8ebe03b0434de2c81375fc6df929e1757f7dd8d7
Akagigahara <leon.wichern@hotmail.com>
2023-12-17 03:01:27 +0100

    /

--- Program.cs
+++ Program.cs
@@ -14,18 +14,19 @@ namespace DiscordBot
 
 			HttpListener listener = new();
 			listener.Prefixes.Add("http://discord.akagigahara.site:80/api/");
-			while (listener.IsListening)
-			{
-				try
-				{ 
-					listener.Start();
-				}
-				catch(Exception ex)
+			try
+			{ 
+				listener.Start();
+				while(listener.IsListening)
 				{
-					Console.WriteLine(ex.ToString());
 					HttpListenerContext context = listener.GetContext();
-					Console.WriteLine(context.Request.ToString);
+					Console.WriteLine(context.Request.ToString());
 				}
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				
 			}
 		}
 	}
