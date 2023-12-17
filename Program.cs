01360ed1151b594e573c4c8920a6f25550c1a297
Akagigahara <leon.wichern@hotmail.com>
2023-12-17 01:39:11 +0100

    Adding HTTPLìstener Support

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace DiscordBot
+﻿using System.Net;
+
+namespace DiscordBot
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, World!");
+			if(!HttpListener.IsSupported)
+			{
+				Console.WriteLine("Not Supported");
+			}
 		}
 	}
 }
