cc2e0d024e16a867f01ea0772ecc1a97bf5d3f14
Akagigahara <lilith.wichern@outlook.com>
2024-02-02 15:44:42 +0100

    -Added ignoring of custom_id on ActionRow

--- Resources/MessageComponents/ActionRow.cs
+++ Resources/MessageComponents/ActionRow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DiscordBot.Resources.MessageComponents
@@ -9,6 +10,8 @@ namespace DiscordBot.Resources.MessageComponents
 	internal class ActionRow : ComponentBase
 	{
 		override public ComponentType type { get; init; } = ComponentType.Action_Row;
+		[JsonIgnore]
+		public override string custom_id { get => base.custom_id; init => base.custom_id = value; }
 		public required ComponentBase[] component { get; init; }
 	}
 }
