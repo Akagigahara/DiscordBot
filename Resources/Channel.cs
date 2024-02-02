7fbe8037ac3bde2844d26251105c38c9bb76ef2d
Akagigahara <lilith.wichern@outlook.com>
2024-02-02 13:20:47 +0100

    - Added Properties to Channel - Added Overwrite class to Channel

--- Resources/Channel.cs
+++ Resources/Channel.cs
@@ -6,11 +6,23 @@
 		public int type { get; set; }
 		public string? guild_id { get; set; }
 		public int position { get; set; }
-		//public Overwrite permission_overwrites { get; set; }
+		public Overwrite? permission_overwrites { get; set; }
 		public string? name { get; set; }
 		public string? topic { get; set; }
 		public bool nsfw { get; set; }
 		public string? last_message_id { get; set; }
+		public int? bitrate { get; set; }
+		public int? user_limit { get; set; }
+		public int? rate_limit_per_user { get; set; }
+		public User[]? recipients { get; set; }
+		public string? icon { get; set; }
+		public string? owner_id { get; set; }
+		public string? application_id { get; set; }
+		public bool managed { get; set; }
+		public string? parent_id { get; set; }
+		public DateTime? last_pin_timestamp { get; set; }
+		public string? rtc_region { get; set; }
+		public int video_quality_mode { get; set; }
 		public enum ChannelType
 		{
 			GUILD_TEXT,
@@ -26,6 +38,14 @@
 			GUILD_DIRECTORY,
 			GUILD_FORUM,
 			GUILD_MEDIA
+		}
+
+		public class Overwrite
+		{
+			public required string id { get; set; }
+			public int type { get; set; }
+			public required string allow { get; set; }
+			public required string deny { get; set; }
 		}
 	}
 }
--- Resources/User.cs
+++ Resources/User.cs
@@ -2,8 +2,12 @@
 {
 	public class User
 	{
-		public required string  id { get; set; }
+		public required string id { get; set; }
 		public required string username { get; set; }
 		public required string discriminator { get; set; }
+		public required string global_name { get; set; }
+		public string? avatar { get; set; }
+		public bool bot { get; set; }
+
 	}
 }
