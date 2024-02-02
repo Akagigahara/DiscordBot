31f1f3bfc015f729f21b621d5476ddecaef9f648
Akagigahara <lilith.wichern@outlook.com>
2024-02-02 15:23:53 +0100

    -Added ActionRow Component -Adjusted Create Response

---
+++ Resources/MessageComponents/ActionRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Resources.MessageComponents
+{
+	internal class ActionRow : ComponentBase
+	{
+		override public ComponentType type { get; init; } = ComponentType.Action_Row;
+		public required ComponentBase[] component { get; init; }
+	}
+}
--- Resources/MessageComponents/ComponentBase.cs
+++ Resources/MessageComponents/ComponentBase.cs
@@ -10,7 +10,7 @@ namespace DiscordBot.Resources.MessageComponents
 	public abstract class ComponentBase
 	{
 		public abstract ComponentType type { get; init; }
-		public virtual required string custom_id { get; init; }
+		public virtual string custom_id { get; init; }
 		public virtual int min_length { get; init; }
 		public virtual int max_length { get; init; }
 		public virtual bool disabled { get; init; }
