b45dcb8081ccff616f34f282e4e93934ad12b547
Lilith Wichern <Lilith.Wichern@outlook.com>
2025-07-19 01:40:24 +0200

    Added foundations for the grid game

---
+++ GridGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    internal class GridGame
+    {
+        private Grid[,] grid = new Grid[10,10];
+        private string currentPicture;
+
+        public GridGame()
+        {
+            string[] collectionOfPictures = Directory.GetFiles("/pictures/");
+
+        }
+
+        private class Grid
+        {
+            bool HasBeenGuessed = false;
+            bool IsTarget = false;
+            Int64 guessedBy;
+        }
+    }
+}
