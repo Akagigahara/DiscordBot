b6f9450e8b295f550e236d78f2f7f4f1e5e96b8c
Akagigahara <lilith.wichern@outlook.com>
2025-07-18 02:51:28 +0200

    Testing Buttons and Modals

--- TestCommand.cs
+++ TestCommand.cs
@@ -1,13 +1,16 @@
-﻿using Discord.Interactions;
+﻿using Discord;
+using Discord.Interactions;
 
 namespace DiscordBot
 {
-    public class TestCommand : InteractionModuleBase<SocketInteractionContext>
-    {
-        [SlashCommand("echo", "Repeats the input you used")]
-        public async Task Echo([Summary(description: "This is the parameter for the text input")]String input)
-        {
-            await RespondAsync(input);
+	public class TestCommand : InteractionModuleBase<SocketInteractionContext>
+	{
+		[SlashCommand("echo", "Repeats the input you used")]
+		public async Task Echo([Summary(description: "This is the parameter for the text input")]string input)
+		{
+			await RespondAsync(input);
+			ComponentBuilder builder = new ComponentBuilder().WithButton("Click me!", "btn-1");
+			await FollowupWithFileAsync("mietzi.jpg", components: builder.Build());
         }
-    }
+	}
 }
