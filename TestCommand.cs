a9850f95aafb39adc694500126535fdf0b21a712
Lilith Wichern <Lilith.Wichern@outlook.com>
2025-07-19 01:39:56 +0200

    Added unit tests

--- TestCommand.cs
+++ TestCommand.cs
@@ -8,7 +8,7 @@ namespace DiscordBot
 		[SlashCommand("echo", "Repeats the input you used")]
 		public async Task Echo([Summary(description: "This is the parameter for the text input")]string input)
 		{
-			await RespondAsync(input);
+			await RespondAsync(input, ephemeral: true);
 			ComponentBuilder builder = new ComponentBuilder().WithButton("Click me!", "btn-1");
 			await FollowupWithFileAsync("mietzi.jpg", components: builder.Build());
         }
