b5664500fed5454d6207b185ce65da8e17e177c4
Lilith Wichern <Lilith.Wichern@outlook.com>
2025-08-13 18:46:14 +0200

    + Added file for Super-Tic-Tac-Toe game

---
+++ Super-3T.cs
@@ -0,0 +1,24 @@
+﻿using Discord.Commands;
+using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    internal class Super_3T
+    {
+    }
+
+    [Discord.Commands.Group("super3t")]
+    public class Super3TCommands : InteractionModuleBase<SocketInteractionContext> 
+    {
+        [SlashCommand("start", "Starts a game of Super Tic-Tac-Toe")]
+        public async Task StartS3T() 
+        {
+
+        }
+    }
+}
